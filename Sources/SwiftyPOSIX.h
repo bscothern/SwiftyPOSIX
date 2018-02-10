f9c04a86cbda47002e66c3e1c8b387c5e19180fa
Braden.Scothern <BradenScothern@gmail.com>
2018-02-09 23:30:21 -0700

    updated README.md, setup carthage support

---
+++ Sources/SwiftyPOSIX.h
@@ -0,0 +1,16 @@
+//
+//  SwiftyPOSIX.h
+//  SwiftyPOSIX
+//
+//  Created by Braden Scothern on 2/9/18.
+//  Copyright © 2018 Braden Scothern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyPOSIX.
+FOUNDATION_EXPORT double SwiftyPOSIXVersionNumber;
+
+//! Project version string for SwiftyPOSIX.
+FOUNDATION_EXPORT const unsigned char SwiftyPOSIXVersionString[];
+
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,14 @@
+//
+//  LinuxMain.swift
+//  SwiftyPOSIXTests
+//
+//  Created by Braden Scothern on 2/9/18.
+//  Copyright © 2018 Braden Scothern. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftyPOSIXTests
+
+XCTMain([
+    testCase(SwiftyPOSIXTests.allTests),
+])
