786380c95d5837222f79b5575137967e7160c734
Braden.Scothern <BradenScothern@gmail.com>
2018-02-09 22:53:55 -0700

    Initial Commit

---
+++ SwiftyPOSIX/SwiftyPOSIX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyPOSIX.h
+//  SwiftyPOSIX
+//
+//  Created by Braden Scothern on 2/9/18.
+//  Copyright © 2018 Braden Scothern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyPOSIX.
+FOUNDATION_EXPORT double SwiftyPOSIXVersionNumber;
+
+//! Project version string for SwiftyPOSIX.
+FOUNDATION_EXPORT const unsigned char SwiftyPOSIXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyPOSIX/PublicHeader.h>
+
+
