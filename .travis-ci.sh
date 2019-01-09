622b282a0358b4b3333a616a84dc8148c9a1f297
Braden.Scothern <BradenScothern@gmail.com>
2019-01-08 23:12:58 -0700

    more adjustments to CI scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
+###################################################################
+# Setup
+###################################################################
 SWIFTLINT=$(which swiftlint || exit 1)
-SWIFT=$(which swift || exit 1)
-XCODEBUILD=$(which xcodebuild || exit 1)
-
-function do_xcodebuild () {
-    ${XCODEBUILD} -version
-    ${XCODEBUILD} -project "${PROJECT}" -scheme "${SCHEME}" -sdk "${SDK}" -destination "${DESTINATION}" -configuration Debug ONLY_ACTIVE_ARCH=NO $@
+function run_swiftlint () {
+    ${SWIFTLINT} --strict
 }
 
-if [[ "${NAME}" == "lint" ]]; then
-    ${SWIFTLINT} --strict;
-elif [[ "${NAME}" == "SwiftPM" ]]; then
-    if [ "${RUN_TESTS}" == "YES" ]; then
+SWIFT=$(which swift || exit 1)
+function run_swift() {
+    if [ ${RUN_TESTS} == "YES" ]; then
         ${SWIFT} test;
     else
         ${SWIFT} build;
     fi
-fi
+}
 
-if [[ ${USE_XCODE_BUILD} != "YES" ]]; then
-    exit 0;
-fi
+XCODEBUILD=$(which xcodebuild || exit 1)
+function run_xcodebuild_args () {
+    if [ ${RUN_TESTS} == "YES" ]; then
+        echo "ENABLE_TESTABILITY=YES test"
+    else
+        echo "build"
+    fi
+}
+function run_xcodebuild () {
+    ${XCODEBUILD} -version
+    ${XCODEBUILD} \
+        -project "${PROJECT}" \
+        -scheme "${SCHEME}" \
+        -sdk "${SDK}" \
+        -destination "${DESTINATION}" \
+        -configuration Debug \
+        ONLY_ACTIVE_ARCH=NO \
+        $(run_xcodebuild_args)
+}
 
-if [ "${RUN_TESTS}" == "YES" ]; then
-    do_xcodebuild ENABLE_TESTABILITY=YES test
-else
-    do_xcodebuild build;
+###################################################################
+# Main Execution
+###################################################################
+if [[ ${NAME} == "lint" ]]; then
+    run_swiftlint
+elif [[ ${NAME} == "SwiftPM" ]]; then
+    run_swift
+elif [[ ${USE_XCODE_BUILD} = "YES" ]]; then
+    run_xcodebuild
 fi
-
