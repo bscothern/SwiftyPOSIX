dcfe56197ac58be6429af0fad8ccf057e2f4f940
Braden.Scothern <BradenScothern@gmail.com>
2019-01-10 22:46:13 -0700

    more before install updates for linux

--- .travis-ci-before-install.sh
+++ .travis-ci-before-install.sh
@@ -4,14 +4,17 @@ if [[ $TRAVIS_OS_NAME != "linux" ]]; then
     exit 0
 fi
 
+SWIFT_VERSION="4.2.1"
+OS_VERSION_SHORT="ubuntu1604"
+OS_VERSION_FULL="ubuntu16.04"
+
 # Setup dependencies
-sudo apt-get install libicu-dev
 wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
 
 # Install Swift
-wget https://swift.org/builds/swift-4.2.1-release/ubuntu1604/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu16.04.tar.gz
-sudo tar xzf swift-4.2.1-RELEASE-ubuntu16.04.tar.gz
-sudo mv swift-4.2.1-RELEASE-ubuntu16.04 /usr/share/swift
+wget https://swift.org/builds/swift-${SWIFT_VERSION}-release/${OS_VERSION_SHORT}/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-${OS_VERSION_FULL}.tar.gz
+sudo tar xzf swift-${SWIFT_VERSION}-RELEASE-${OS_VERSION_FULL}.tar.gz
+sudo mv swift-${SWIFT_VERSION}-RELEASE-${OS_VERSION_FULL} /usr/share/swift
 
 # Setup env
 echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
