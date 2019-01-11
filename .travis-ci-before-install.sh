497051a4f32cdfc4ae02f94b41f06a3d4806ec6c
Braden.Scothern <BradenScothern@gmail.com>
2019-01-10 23:23:58 -0700

    more debugging...

--- .travis-ci-before-install.sh
+++ .travis-ci-before-install.sh
@@ -14,3 +14,8 @@ wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
 # Install Swift
 wget https://swift.org/builds/swift-${SWIFT_VERSION}-release/${OS_VERSION_SHORT}/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-${OS_VERSION_FULL}.tar.gz
 sudo tar xzf swift-${SWIFT_VERSION}-RELEASE-${OS_VERSION_FULL}.tar.gz -v -C /
+
+ls /usr/bin
+ls /usr
+ls /
+
