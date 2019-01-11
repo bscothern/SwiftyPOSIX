171ee64442e9494f89dafe4f3023bde6559657f1
Braden.Scothern <BradenScothern@gmail.com>
2019-01-10 22:42:25 -0700

    working on more linux CI

--- .travis-ci-before-install.sh
+++ .travis-ci-before-install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [[ $TRAVIS_OS_NAME != "linux" ]]; then
     exit 0
 fi
 
 # Setup dependencies
-sudo apt-get install clang libicu-dev libcurl3 libpython2.7 libpython2.7-dev 
+sudo apt-get install libicu-dev
 wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
 
 # Install Swift
