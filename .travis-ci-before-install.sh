2cc12aef6b3ce4d4128396c847cf98a581a07287
Braden.Scothern <BradenScothern@gmail.com>
2019-01-10 22:34:26 -0700

    working on getting Linux CI working

--- .travis-ci-before-install.sh
+++ .travis-ci-before-install.sh
@@ -11,7 +11,7 @@ wget -q -O - https://swift.org/keys/all-keys.asc | sudo gpg --import -
 # Install Swift
 wget https://swift.org/builds/swift-4.2.1-release/ubuntu1604/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu16.04.tar.gz
 sudo tar xzf swift-4.2.1-RELEASE-ubuntu16.04.tar.gz
-mv swift-4.2.1-RELEASE-ubuntu16.04 /usr/share/swift
+sudo mv swift-4.2.1-RELEASE-ubuntu16.04 /usr/share/swift
 
 # Setup env
 echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
