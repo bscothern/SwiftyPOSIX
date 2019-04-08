5a9467fb8e90787fa29ee1c1e6155d8b56555ea3
Braden Scothern <BradenScothern@gmail.com>
2019-04-08 16:49:56 -0600

    fixing build issues

--- Package.swift
+++ Package.swift
@@ -33,18 +33,13 @@ import PackageDescription
 let package = Package(
     name: "SwiftyPOSIX",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyPOSIX",
             targets: ["SwiftyPOSIX_C", "SwiftyPOSIX"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyPOSIX_C",
             dependencies: [],
@@ -56,5 +51,6 @@ let package = Package(
         .testTarget(
             name: "SwiftyPOSIXTests",
             dependencies: ["SwiftyPOSIX"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v4_2, .v5]
 )
